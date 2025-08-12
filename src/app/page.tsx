2b532d69b76abbc762e127526a217e96958ff854
Yuri Cruz <yuricruzf@gmail.com>
2025-08-11 23:20:05 -0300

    Creating server and displaying posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,40 @@
 import CardPost from "@/components/CardPost";
+import { Post } from "@/types";
 
 // import Image from "next/image";
-const post = {
-  "id": 1,
-  "cover": "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/posts/introducao-ao-react.png",
-  "title": "Introdução ao React",
-  "slug": "introducao-ao-react",
-  "body": "Neste post, vamos explorar os conceitos básicos do React, uma biblioteca JavaScript para construir interfaces de usuário. Vamos cobrir componentes, JSX e estados.",
-  "markdown": "```javascript\nfunction HelloComponent() {\n  return <h1>Hello, world!</h1>;\n}\n```",
-  "author": {
-    "id": 101,
-    "name": "Ana Beatriz",
-    "username": "anabeatriz_dev",
-    "avatar": "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/authors/anabeatriz_dev.png",
-    "imageAlt": "Imagem de Ana Beatriz",
-    "userName": "anabeatriz_dev"
-  },
-  "altImg": "Imagem de Ana Beatriz"
+// const post = {
+//   "id": 1,
+//   "cover": "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/posts/introducao-ao-react.png",
+//   "title": "Introdução ao React",
+//   "slug": "introducao-ao-react",
+//   "body": "Neste post, vamos explorar os conceitos básicos do React, uma biblioteca JavaScript para construir interfaces de usuário. Vamos cobrir componentes, JSX e estados.",
+//   "markdown": "```javascript\nfunction HelloComponent() {\n  return <h1>Hello, world!</h1>;\n}\n```",
+//   "author": {
+//     "id": 101,
+//     "name": "Ana Beatriz",
+//     "username": "anabeatriz_dev",
+//     "avatar": "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/authors/anabeatriz_dev.png",
+//     "imageAlt": "Imagem de Ana Beatriz",
+//     "userName": "anabeatriz_dev"
+//   },
+//   "altImg": "Imagem de Ana Beatriz"
+// }
+
+async function getAllPost() {
+  const response = await fetch('http://localhost:3042/posts')
+    .then(res => {
+      if (!res.ok) throw new Error('Erro ao buscar posts')
+      return res.json()
+    })
+  return response
 }
-export default function Home() {
+
+export default async function Home() {
+  const posts = await getAllPost()
   return (
     <div>
       <main>
-        <CardPost post={post}/>
+        {posts.map((post: Post) => <CardPost key={post.id} post={post} />)}
       </main>
     </div>
   );
