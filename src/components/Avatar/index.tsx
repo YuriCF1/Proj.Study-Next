66b0f49fde5f9fa204b0ac5f1c293ddec08b517c
Yuri Cruz <yuricruzf@gmail.com>
2025-08-11 22:48:19 -0300

    Setting css

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
+import styles from './avatar.module.css'
 
 const Avatar = ({ name, imageSrc, imageAlt }: { name: string, imageSrc: string, imageAlt: string }) => {
 // const Avatar = ({ name, imageSrc }: { name: string, imageSrc: string }) => {
   return (
-    <ul>
+    <ul className={styles.avatar}>
       <li>
         <Image src={imageSrc} alt={imageAlt} width={32} height={32} />
       </li>
--- src/components/CardPost/index.tsx
+++ src/components/CardPost/index.tsx
@@ -1,19 +1,20 @@
 import { CardPostProps } from '../../types';
 import Image from 'next/image'
 import Avatar from '../Avatar';
+import styles from './cardPost.module.css';
 
 const CardPost = ({ post }: CardPostProps) => {
   return (
-    <article>
-      <header>
+    <article className={styles.card}>
+      <header className={styles.header}>
         <figure>
           <Image src={post.cover} alt={`Imagem de ${post.title}`} width={438} height={133} />
         </figure>
       </header>
-      <section>
+      <section className={styles.body}>
         <h2>{post.title}</h2>
         <p>{post.body}</p>
-        <footer>
+        <footer className={styles.footer}>
           {/* <Avatar name={post.author.userName} imageSrc={post.author.avatar} imageAlt={post.author.userName} /> */}
           <Avatar name={post.author.userName} imageSrc={post.author.avatar} imageAlt={`Imagem da ${post.author.userName}`} />
         </footer>
